e141525b4277a2d19c4a3036148207088d6465dd
isaacpesqueira <52761199+isaacpesqueira@users.noreply.github.com>
2020-01-10 00:50:20 -0800

    Add files via upload

---
+++ login/script.js
@@ -0,0 +1,49 @@
+function create()
+{
+ var password=document.getElementById("password").value; 
+  
+ var email=document.getElementById("email").value; 
+  
+ var retype=document.getElementById("retype").value; 
+      emailRegex = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
+
+  if (emailRegex.test(email))
+{
+  
+  if(retype==password  )
+     {
+   
+       
+   document.getElementById("signup").innerHTML="processing";    
+     
+     }
+  else
+    {
+      alert("passwords do not match");
+    }
+ }
+  else
+    {
+      alert("Wrong Email");
+    }
+}
+
+
+function toggleSignup(){
+   document.getElementById("login-toggle").style.backgroundColor="#fff";
+    document.getElementById("login-toggle").style.color="#222";
+    document.getElementById("signup-toggle").style.backgroundColor="#57b846";
+    document.getElementById("signup-toggle").style.color="#fff";
+    document.getElementById("login-form").style.display="none";
+    document.getElementById("signup-form").style.display="block";
+}
+
+function toggleLogin(){
+    document.getElementById("login-toggle").style.backgroundColor="#57B846";
+    document.getElementById("login-toggle").style.color="#fff";
+    document.getElementById("signup-toggle").style.backgroundColor="#fff";
+    document.getElementById("signup-toggle").style.color="#222";
+    document.getElementById("signup-form").style.display="none";
+    document.getElementById("login-form").style.display="block";
+}
+
