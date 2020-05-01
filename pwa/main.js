5b3d02dfa6b5bf354a190f9d3de7762679f12195
isaacpesqueira <52761199+isaacpesqueira@users.noreply.github.com>
2020-05-01 13:33:51 -0700

    service worker

--- pwa/main.js
+++ pwa/main.js
@@ -5,8 +5,10 @@
 if('serviceWorker' in navigator)
 {
 console.log('YES !! SERVICE WORKER EXISTS');	
-
-
+ 
+navigator.serviceWorker.register('pwa/sw.js')
+					.then(res => console.log('Service Worker Cargado Correctamente',res))
+					.catch(err=> console.log("No se logro",err));
 }
 else
 {
