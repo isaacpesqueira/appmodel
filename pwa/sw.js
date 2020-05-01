98cf3ea8df84b96af1364bd652b51c0d689e236a
isaacpesqueira <52761199+isaacpesqueira@users.noreply.github.com>
2020-05-01 14:41:42 -0700

    MANI

--- pwa/sw.js
+++ pwa/sw.js
@@ -4,26 +4,26 @@ const CACHE_NAME = 'v1_cache_appmodel_pwa';
 
 //ficheros a cachear en la app
 var urltoCache = [
-'./',
-'./images/btn_whatsapp.png',
-'./images/pizza.png',
-'./assets/bootstrap/css/bootstrap.min.css',
-'./assets/css/styles.min.css',
-'./assets/img/glass.jpg',
-'./assets/img/landing-free-landing-page-website-template.jpg',
-'./assets/img/login.PNG',
-'./assets/img/negocio.PNG',
-'./assets/img/office.jpg',
-'./assets/img/online-store.png',
-'./assets/img/online-store.png',
-'./assets/img/responsive-1622825_640.png',
-'./assets/js/script.min.js',
+'../',
+'../images/btn_whatsapp.png',
+'../images/pizza.png',
+'../assets/bootstrap/css/bootstrap.min.css',
+'../assets/css/styles.min.css',
+'../assets/img/glass.jpg',
+'../assets/img/landing-free-landing-page-website-template.jpg',
+'../assets/img/login.PNG',
+'../assets/img/negocio.PNG',
+'../assets/img/office.jpg',
+'../assets/img/online-store.png',
+'../assets/img/online-store.png',
+'../assets/img/responsive-1622825_640.png',
+'../assets/js/script.min.js',
 
-'./precios.html',
-'./catalogo.html',
-'./contact.html',
-'./negocios.html',
-'./precios.html'
+'../precios.html',
+'../catalogo.html',
+'../contact.html',
+'../negocios.html',
+'../precios.html'
 
 
 ];
@@ -67,7 +67,7 @@ self.addEventListener('activate',e=>{
 								return caches.delete(cacheName);
 						}	
 			
-				})
+				});
 );
 					
 		})
