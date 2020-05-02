6b6e0dab542131d1b90bd8b7196a583ce85e598c
isaacpesqueira <52761199+isaacpesqueira@users.noreply.github.com>
2020-05-01 20:53:36 -0700

    Update sw.js

--- pwa/sw.js
+++ pwa/sw.js
@@ -100,6 +100,7 @@ self.addEventListener("activate", event => {
 // if we have them
 self.addEventListener('fetch', function(event) {
     console.log("Event:Fetch");
+    notifyMe();
     if (doCache) {
 
       event.respondWith(
@@ -109,3 +110,30 @@ self.addEventListener('fetch', function(event) {
       );
     }
 });
+
+
+function notifyMe() {
+  // Comprobamos si el navegador soporta las notificaciones
+  if (!("Notification" in window)) {
+    alert("Este navegador no soporta las notificaciones del sistema");
+  }
+
+  // Comprobamos si ya nos habían dado permiso
+  else if (Notification.permission === "granted") {
+    // Si esta correcto lanzamos la notificación
+    var notification = new Notification("Holiwis :D");
+  }
+
+  // Si no, tendremos que pedir permiso al usuario
+  else if (Notification.permission !== 'denied') {
+    Notification.requestPermission(function (permission) {
+      // Si el usuario acepta, lanzamos la notificación
+      if (permission === "granted") {
+        var notification = new Notification("Gracias majo!");
+      }
+    });
+  }
+
+  // Finalmente, si el usuario te ha denegado el permiso y 
+  // quieres ser respetuoso no hay necesidad molestar más.
+}
