d8e4c7501c720bd3b32cfbbd7553bf81435294f1
isaacpesqueira <52761199+isaacpesqueira@users.noreply.github.com>
2020-05-03 16:52:22 -0700

    Update sw.js

--- pwa/sw.js
+++ pwa/sw.js
@@ -58,9 +58,7 @@ if (doCache) {
 
 self.addEventListener("activate", event => {
       console.log("Event:Activate");
-     Notification.requestPermission();
-
-
+  
  for (var i = 0; i < 1000; i++) {
    setTimeout(function(){ console.log("Tengo el Poder Hello, service worker");
     
