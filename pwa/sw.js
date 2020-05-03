49b3518667a93d458a055dc8401e6e9ef2c38031
isaacpesqueira <52761199+isaacpesqueira@users.noreply.github.com>
2020-05-02 18:51:49 -0700

    Update sw.js

--- pwa/sw.js
+++ pwa/sw.js
@@ -88,30 +88,10 @@ self.addEventListener('fetch', function(event) {
 });
 
 
-self.addEventListener('push', function(event) {
-  if (!(self.Notification && self.Notification.permission === 'granted')) {
-    return;
-  }
+elf.addEventListener('push', function(event) {
+  const promiseChain = self.registration.showNotification('Hello, World.');
 
-  var data = {};
-  if (event.data) {
-    data = event.data.json();
-  }
-  var title = data.title || "Something Has Happened";
-  var message = data.message || "Here's something you might want to check out.";
-  var icon = "images/new-notification.png";
-
-  var notification = new self.Notification(title, {
-    body: message,
-    tag: 'simple-push-demo-notification',
-    icon: icon
-  });
-
-  notification.addEventListener('click', function() {
-    if (clients.openWindow) {
-      clients.openWindow('https://example.blog.com/2015/03/04/something-new.html');
-    }
-  });
+  event.waitUntil(promiseChain);
 });
 
 
